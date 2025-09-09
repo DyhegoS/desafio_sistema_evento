855fa2bd979e28f63c9d8d870b574b784cbd67ce
DyhegoS <dyhegodasilva@gmail.com>
2025-09-09 18:09:33 -0300

    implementação das entidades e das annotations

---
+++ src/main/java/com/formacaojavaspring/sistemaevento/entities/Categoria.java
@@ -0,0 +1,49 @@
+package com.formacaojavaspring.sistemaevento.entities;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String descricao;
+
+    @OneToMany(mappedBy = "categoria")
+    private Set<Atividade> atividades = new HashSet<>();
+    
+    public Categoria(Integer id, String description) {
+        this.id = id;
+        this.descricao = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String description) {
+        this.descricao = description;
+    }
+
+    public Set<Atividade> getAtividades() {
+        return atividades;
+    }
+}
